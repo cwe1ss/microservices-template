5b0daaf530552783634b4e183480831c570274bb
Christian Weiss <christian@chwe.at>
2022-08-25 16:40:06 +0200

    create resource group in script

--- infrastructure/deploy-platform.ps1
+++ infrastructure/deploy-platform.ps1
@@ -3,11 +3,21 @@ $location = "westeurope"
 
 . .\deploy-helpers.ps1
 
-"Deploying platform"
+# Naming conventions
+$platformGroupName = "$platformResourcePrefix-platform"
+$tags = @("product=$platformResourcePrefix")
 
+"Platform resource group"
 Exec {
-    az deployment sub create `
-        --location $location `
-        --template-file .\platform.bicep `
-        --parameters location=$location platformResourcePrefix=$platformResourcePrefix
+    az group create --name $platformGroupName --location $location --tags $tags -o none
+}
+
+"Platform resources"
+Exec {
+    az deployment group create `
+        --resource-group $platformGroupName `
+        --name ("platform-" + (Get-Date).ToString("yyyyMMddHHmmss")) `
+        --output none `
+        --template-file .\platform-resources.bicep `
+        --parameters platformResourcePrefix=$platformResourcePrefix
 }
