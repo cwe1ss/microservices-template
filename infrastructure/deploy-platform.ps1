217ce7a2c9754506c41585b7b5f054d31de452a4
Christian Weiss <christian@chwe.at>
2022-09-04 21:29:14 +0200

    clean up

--- infrastructure/deploy-platform.ps1
+++ infrastructure/deploy-platform.ps1
@@ -16,6 +16,7 @@ $githubAppName = "$($config.platformResourcePrefix)-github"
 "Loading GitHub application from Azure AAD"
 
 $githubSp = Get-AzADServicePrincipal -DisplayName $githubAppName
+if (!$githubSp) { throw "Service principal '$githubAppName' not found. Did you run 'init-platform.ps1'?" }
 
 
 ############################
