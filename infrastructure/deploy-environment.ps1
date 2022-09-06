39a99d186e7f55ba039b782191e3c3273c8c58a3
Christian Weiss <christian@chwe.at>
2022-09-06 09:53:12 +0200

    testing deployment with new PS version

--- infrastructure/deploy-environment.ps1
+++ infrastructure/deploy-environment.ps1
@@ -58,7 +58,7 @@ New-AzSubscriptionDeployment `
 Start-Sleep -Seconds (6*60)
 ".... done"
 
-Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com/" | Out-Null
+#Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com/" | Out-Null
 
 
 ############################
