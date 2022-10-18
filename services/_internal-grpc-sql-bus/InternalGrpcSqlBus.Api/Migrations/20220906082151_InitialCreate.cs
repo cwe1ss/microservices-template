5c5a0887d1c05c36c565d982f090924496dc59eb
Christian Weiss <christian@chwe.at>
2022-10-18 17:33:44 +0200

    clean up: use file scoped namespace

--- services/_internal-grpc-sql-bus/InternalGrpcSqlBus.Api/Migrations/20220906082151_InitialCreate.cs
+++ services/_internal-grpc-sql-bus/InternalGrpcSqlBus.Api/Migrations/20220906082151_InitialCreate.cs
@@ -2,32 +2,31 @@
 
 #nullable disable
 
-namespace InternalGrpcSqlBus.Api.Migrations
+namespace InternalGrpcSqlBus.Api.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Customers",
-                columns: table => new
-                {
-                    CustomerId = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
-                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
-                });
-        }
+        migrationBuilder.CreateTable(
+            name: "Customers",
+            columns: table => new
+            {
+                CustomerId = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Customers", x => x.CustomerId);
+            });
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Customers");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Customers");
     }
 }
